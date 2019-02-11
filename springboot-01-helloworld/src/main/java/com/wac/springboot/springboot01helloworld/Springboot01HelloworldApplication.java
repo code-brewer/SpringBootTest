27ef6808d2f3c9e9eb19637f92436a902ceeb564
WangAncheng <1515476572@qq.com>
2019-02-11 22:51:10 +0800

    helloworld

--- springboot-01-helloworld/src/main/java/com/wac/springboot/springboot01helloworld/Springboot01HelloworldApplication.java
+++ springboot-01-helloworld/src/main/java/com/wac/springboot/springboot01helloworld/Springboot01HelloworldApplication.java
@@ -2,8 +2,9 @@ package com.wac.springboot.springboot01helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Springboot01HelloworldApplication {
 
     public static void main(String[] args) {
